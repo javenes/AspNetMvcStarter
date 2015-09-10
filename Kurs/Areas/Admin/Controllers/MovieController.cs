c70625d017893ea7cc6abd31b33d2c70b885853e
javenes <jarle@javenes.no>
2015-09-10 16:27:59 +0200

    Etter dag 4.

--- Kurs/Areas/Admin/Controllers/MovieController.cs
+++ Kurs/Areas/Admin/Controllers/MovieController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kurs.Areas.Admin.Models;
+using MovieDAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,68 @@ namespace Kurs.Areas.Admin.Controllers
 {
     public class MovieController : Controller
     {
+        ImdbContext db = new ImdbContext();
+
         // GET: Admin/Movie
-        public string Index()
+        public ActionResult Index()
         {
-            return "Admin.MovieController.Index()";
+            ViewData.Model = db.Movies;
+
+            return View();
         }
+
+        public ActionResult Create()
+        {
+            ViewBag.Genres = new SelectList(db.Genres, "GenreId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MovieX model)
+        {
+            ViewBag.Genres = new SelectList(db.Genres, "GenreId", "Name");
+
+            if (ModelState.IsValid)
+            {
+
+                var movie = new Movie();
+                movie.MovieId = model.MovieId;
+
+                movie.OriginalTitle = model.OriginalTitle;
+                movie.Title = model.Title;
+                movie.Description = model.Description;
+                movie.RunningLength = (model.RunningLengthHour * 60) + model.RunningLengthMinute;
+
+                if (movie.RunningLength < 10)
+                {
+                    ModelState.AddModelError("RunningLengthHour", "Filmen er for kort");
+                    ModelState.AddModelError("RunningLengthMinutes", "Filmen er for kort");
+                    return View();
+                }
+                movie.ProductionYear = model.ProductionYear;
+                movie.Genre = db.Genres.Find(model.GenreId);
+
+                db.Movies.Add(movie);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Movie");
+
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(string movieId)
+        {
+            var m = db.Movies.Find(movieId);
+            if (m == null)
+                return HttpNotFound();
+
+            ViewData.Model = new MovieX
+            return View("Index");
+        }
+
+
     }
 }
---
+++ Kurs/Areas/Admin/Models/MovieX.cs
@@ -0,0 +1,50 @@
+﻿using MovieDAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kurs.Areas.Admin.Models
+{
+
+    public class MovieX
+    {
+        [CustomValidation (typeof(MovieValidatorClass), "NewMovie")]
+        [Required]
+        [MaxLength(30)]
+        public string MovieId { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(100)]
+        public string OriginalTitle { get; set; }
+
+        public string Description { get; set; }
+
+        [MaxLength(4)]
+        public string ProductionYear { get; set; }
+
+        public int RunningLengthHour { get; set; }
+
+        public int RunningLengthMinute { get; set; }
+
+        public int GenreId { get; set; }
+
+    }
+
+    public class MovieValidatorClass
+    {
+        public static ValidationResult NewMovie(string movieId)
+        {
+            ImdbContext db = new ImdbContext();
+            if (db.Movies.Find(movieId) == null)
+            {
+                return ValidationResult.Success;
+            }
+            else
+                return new ValidationResult("Filmen finnes fra før");
+        }
+    }
+}
